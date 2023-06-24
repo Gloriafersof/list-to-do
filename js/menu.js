5f47ff8285e638bb4f4a43eb864868e9ca8bd5ad
Gloriafersof <gloriafersof2013@gmail.com>
2023-06-24 10:50:50 -0400

    agregando

--- js/menu.js
+++ js/menu.js
@@ -44,8 +44,10 @@ const butomesa = document.getElementById("data-boton");
 const optionmesa1= document.getElementById("informacion-mesa1");
 const optionmesa2= document.getElementById("informacion-mesa2");
 const optionmesa3=document.getElementById("informacion-mesa3");
-
-
+const optionmesa4=document.getElementById("informacion-mesa4");
+const optionmesa5=document.getElementById("informacion-mesa5");
+const optionmesa6=document.getElementById("informacion-mesa6");
+const optionmesa7=document.getElementById("informacion-mesa7");
 
 
 
@@ -59,6 +61,10 @@ mesa1.addEventListener('click',() =>{
     optionmesa1.classList.toggle("muestra");
     optionmesa2.classList.remove('muestra');
     optionmesa3.classList.remove('muestra');
+    optionmesa4.classList.remove("muestra");
+    optionmesa5.classList.remove('muestra'); 
+    optionmesa6.classList.remove('muestra');
+    optionmesa7.classList.remove('muestra');
 
         
 
@@ -74,6 +80,10 @@ mesa2.addEventListener('click',() =>{
     optionmesa2.classList.toggle("muestra");
     optionmesa1.classList.remove('muestra')
     optionmesa3.classList.remove('muestra')
+    optionmesa4.classList.remove("muestra");
+    optionmesa5.classList.remove('muestra'); 
+    optionmesa6.classList.remove('muestra');
+    optionmesa7.classList.remove('muestra');
 
         
 
@@ -85,11 +95,75 @@ mesa3.addEventListener('click',() =>{
 
     butomesa.classList.toggle("quita");
     optionmesa3.classList.toggle("muestra");
-    optionmesa1.classList.remove('muestra')
-    optionmesa2.classList.remove('muestra')
+    optionmesa1.classList.remove('muestra');
+    optionmesa2.classList.remove('muestra');
+    optionmesa4.classList.remove("muestra");
+    optionmesa5.classList.remove('muestra'); 
+    optionmesa6.classList.remove('muestra');
+    optionmesa7.classList.remove('muestra');
         
 
 })
 
+const mesa4 = document.getElementById('data-boton4')
+
+mesa4.addEventListener('click',() =>{
+
+    butomesa.classList.toggle("quita");
+    optionmesa4.classList.toggle("muestra");
+    optionmesa3.classList.remove("muestra");
+    optionmesa1.classList.remove('muestra');
+    optionmesa2.classList.remove('muestra');
+    optionmesa5.classList.remove('muestra'); 
+    optionmesa6.classList.remove('muestra');
+    optionmesa7.classList.remove('muestra');
+        
+
+})
+
+const mesa5 = document.getElementById('data-boton5')
+
+mesa5.addEventListener('click',() =>{
+
+    butomesa.classList.toggle("quita");
+    optionmesa5.classList.toggle("muestra");
+    optionmesa3.classList.remove("muestra");
+    optionmesa1.classList.remove('muestra');
+    optionmesa2.classList.remove('muestra');
+    optionmesa4.classList.remove('muestra');   
+    optionmesa6.classList.remove('muestra'); 
+    optionmesa7.classList.remove('muestra');  
+})
 
 
+
+const mesa6 = document.getElementById('data-boton6')
+
+mesa6.addEventListener('click',() =>{
+
+    butomesa.classList.toggle("quita");
+    optionmesa6.classList.toggle("muestra");
+    optionmesa3.classList.remove("muestra");
+    optionmesa1.classList.remove('muestra');
+    optionmesa2.classList.remove('muestra');
+    optionmesa4.classList.remove('muestra');   
+    optionmesa5.classList.remove('muestra'); 
+    optionmesa7.classList.remove('muestra');
+
+    
+})
+
+
+const mesa7 = document.getElementById('data-boton7')
+
+mesa7.addEventListener('click',() =>{
+
+    butomesa.classList.toggle("quita");
+    optionmesa7.classList.toggle("muestra");
+    optionmesa3.classList.remove("muestra");
+    optionmesa1.classList.remove('muestra');
+    optionmesa2.classList.remove('muestra');
+    optionmesa4.classList.remove('muestra');   
+    optionmesa5.classList.remove('muestra'); 
+    optionmesa6.classList.remove('muestra'); 
+})
